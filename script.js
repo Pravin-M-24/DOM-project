a0e5e530da8f4c35932a2d36ff3e6614b8a9426e
Your Name <you@example.com>
2024-01-11 16:22:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let box = document.getElementById("container");
+box.style.backgroundColor = "yellow";
+box.style.margin = "20px";
+box.style.padding = "10px";
+box.style.fontSize = "18px";
+box.style.height ="200px";
+box.style.width="300px";
+box.style.color ="blue";
+box.style.fontWeight="600";
